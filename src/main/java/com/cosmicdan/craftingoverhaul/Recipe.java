3763deaa91edcbc308e6791d1b2eb15fcdfeba06
CosmicDan <dconnolly.au@gmail.com>
2014-11-29 08:18:27 +1100

    Dynamically assign recipe categories (horray for reflection)
    The performance of Class.forName might worry me in usual cases, but this is all done only once on player login so meh.

--- src/main/java/com/cosmicdan/craftingoverhaul/Recipe.java
+++ src/main/java/com/cosmicdan/craftingoverhaul/Recipe.java
@@ -3,7 +3,8 @@ package com.cosmicdan.craftingoverhaul;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.item.*;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
 
 public class Recipe {
     public String recipeName;
@@ -12,7 +13,7 @@ public class Recipe {
     public ItemStack recipeOutput;
     public RecipeHandler.RecipeSize recipeType;
     public RecipeHandler.RecipeStyles recipeStyle;
-    public Class recipeClass;
+    public Class recipeClass; // this may only be useful for debugging, not certain at this point
     public RecipeHandler.RecipeCategories recipeCategory;
     
     
@@ -25,38 +26,64 @@ public class Recipe {
                   Class recipeClass) {
         this.recipeName = recipeName;
         this.recipeLabel = recipeLabel;
-        this.recipeInput = recipeInput; // remember to check if it's an instance of ItemStack before reading this
+        this.recipeInput = recipeInput; // remember to check if it's an instance of ItemStack before reading this (some Recipes use null instead of "")
         this.recipeOutput = recipeOutput; 
         this.recipeType = recipeType;
         this.recipeStyle = recipeStyle;
-        this.recipeClass = recipeClass; // this may only be useful for debugging, not certain at this point
+        this.recipeClass = recipeClass;
         this.recipeCategory = setRecipeCategory(recipeOutput.getItem());
     }
     
+
+
     private RecipeHandler.RecipeCategories setRecipeCategory(Object recipeOutputItem) {
-        // I don't like this code *at all*. Surely there's a more readable way to do this.
-        // TODO: Implement some "intelligent" category detection based on heuristic reflection.
-        if (recipeOutputItem instanceof ItemArmor)
+        // TODO: Export the class <> category mappings to a plain-text file
+        // TODO: Convert the RecipeCategories enum to a new object, allowing the categories 
+        //       to be exported too. See GotoLink's suggestion here-
+        //       http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/modification-development/2289243-dynamically-refer-to-a-class-reflection-when?comment=3
+        if (isInstanceOf(recipeOutputItem, "net.minecraft.item.ItemArmor"))
             return RecipeHandler.RecipeCategories.ARMOR;
-        if (recipeOutputItem instanceof net.minecraftforge.common.IPlantable)
+        if (isInstanceOf(recipeOutputItem, "net.minecraftforge.common.IPlantable"))
             return RecipeHandler.RecipeCategories.CROPS;
-        if (recipeOutputItem instanceof ItemFood)
+        if (isInstanceOf(recipeOutputItem, "net.minecraft.item.ItemFood"))
             return RecipeHandler.RecipeCategories.FOOD;
-        if (recipeOutputItem instanceof ItemMapBase)
+        if (isInstanceOf(recipeOutputItem, "net.minecraft.item.ItemMapBase"))
             return RecipeHandler.RecipeCategories.MAPPING;
-        if ((recipeOutputItem instanceof ItemTool) ||
-                (recipeOutputItem instanceof ItemShears) ||
-                (recipeOutputItem instanceof ItemHoe) ||
-                (recipeOutputItem instanceof ItemFlintAndSteel) ||
-                (recipeOutputItem instanceof ItemFishingRod)
-                )
+        if (isInstanceOf(recipeOutputItem, "net.minecraft.item.ItemTool", 
+                                           "net.minecraft.item.ItemShears", 
+                                           "net.minecraft.item.ItemHoe", 
+                                           "net.minecraft.item.ItemFlintAndSteel", 
+                                           "net.minecraft.item.ItemFishingRod"
+                                           ))
             return RecipeHandler.RecipeCategories.TOOLS;
-        if ((recipeOutputItem instanceof ItemSword) ||
-                (recipeOutputItem instanceof ItemBow)
-                )
+        if (isInstanceOf(recipeOutputItem, "net.minecraft.item.ItemSword", 
+                                           "net.minecraft.item.ItemBow"
+                                           ))
             return RecipeHandler.RecipeCategories.WEAPONS;
-        if (recipeOutputItem instanceof ItemBlock) // check for blocks last, since other types can extend it
+        if (isInstanceOf(recipeOutputItem, "net.minecraft.item.ItemBlock")) 
+            // we check for blocks last because other types may extend it
             return RecipeHandler.RecipeCategories.BLOCKS;
         return RecipeHandler.RecipeCategories.OTHER; // default
+    }
+    
+    private static boolean isInstanceOf(Object item, String... classPaths) {
+        Class matchedClass;
+        for (String classPath : classPaths) {
+            try {
+                matchedClass = Class.forName(getClassName(classPath));
+                if (matchedClass.isInstance(item)) return true;
+            } catch (Exception e) {
+                // no item class, use generic category
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
+    }
+    
+    private static String getClassName(String cls) {
+        if (FMLDeobfuscatingRemapper.INSTANCE.isRemappedClass(cls))
+            return FMLDeobfuscatingRemapper.INSTANCE.unmap(cls.replace('.', '/'));
+        return cls; // default
     }
 }
